313a6863ca5057350565a71de64ee476e4d68eb5
Bodzionek <pebe83@gmail.com>
2026-02-23 18:51:12 +0100

    Updated healt bar

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771868260|15024665';
+const CACHE_VERSION = '1771869053|807679210';
 /** @type {string} */
 const CACHE_PREFIX = '2d survival-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
