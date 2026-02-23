187c6b1be7ff544c2595f9a7476bcc5e886cf2f1
Bodzionek <pebe83@gmail.com>
2026-02-23 18:30:53 +0100

    added healing on level up

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771865715|1978168437';
+const CACHE_VERSION = '1771867810|4072713085';
 /** @type {string} */
 const CACHE_PREFIX = '2d survival-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
