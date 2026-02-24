46199aa979fa0fc0a3722452496c5acd9ddf29fa
Bodzionek <pebe83@gmail.com>
2026-02-24 20:18:50 +0100

    Update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771869053|807679210';
+const CACHE_VERSION = '1771960705|85025647';
 /** @type {string} */
 const CACHE_PREFIX = '2d survival-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
